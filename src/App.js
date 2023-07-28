9bb117080f48e67032dda7d33adef0ad3330cc92
NoEasyWorld <70003713+apple3243@users.noreply.github.com>
2023-07-28 14:55:46 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
 
-// 1. How to use the hook "useInput"
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value }
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
-  };
-  return { value, onChange };
-};
-
 export default function App() {
-  // const maxLen = (value) => value.length <= 10; //No return if the value is more than 10
-  const Include = (value) => !value.includes("@"); //No return if the value includes @
-  const name = useInput("Mr.", Include);
+  const sayHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+  useEffect(sayHello, [number]); // sayHello executes only when number(dependency) changes
   return (
     <div className="App">
       <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 }
