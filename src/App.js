bec5f215225e355aa73f167ccfe65f3837d75f8d
NoEasyWorld <70003713+apple3243@users.noreply.github.com>
2023-08-11 12:12:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
-// 1. How to use the hook "useInput"
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value }
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
   };
-  return { value, onChange };
+  useEffect(updateTitle, [title]);
+  return setTitle;
 };
 
 export default function App() {
-  // const maxLen = (value) => value.length <= 10; //No return if the value is more than 10
-  const Include = (value) => !value.includes("@"); //No return if the value includes @
-  const name = useInput("Mr.", Include);
+  const titleUpdater = useTitle("Loading...");
   return (
     <div className="App">
       <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
     </div>
   );
 }
+
