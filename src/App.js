38cef6e0ac7854c037a6eac52828a582a9e548ae
NoEasyWorld <70003713+apple3243@users.noreply.github.com>
2023-08-16 15:22:24 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
-const useClick = (onClick) => {
-  // useRef is a React Hook that lets you reference a value that’s not needed for rendering.
-  const element = useRef();
+const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0
+  });
+  const onScroll = () => {
+    setState({ y: window.scrollY, x: window.scrollX });
+  };
   useEffect(() => {
-    // useEffect will be called when it is in the state of componentDidmount, conponentDidUpdate
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
-    }
-    // Need to clean up EventListener when it is unmount ~
-    // This function with return will be called when it is in the state of componentWillUnMount
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
-    };
-    // If there had dependency, the function will be called only when it is in the state of componentDidmount
-  }, []);
-  return element;
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  });
+  return state;
 };
+
 export default function App() {
-  const sayHello = () => (title.current.innerText = "Say Hello");
-  const title = useClick(sayHello);
+  const { y } = useScroll();
   return (
-    <div className="App">
-      <h1 ref={title}>Hello</h1>
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
+        Hello
+      </h1>
     </div>
   );
 }
