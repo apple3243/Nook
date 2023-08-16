54a06923b483b8a5c46aad9fdeda71e4f042215c
NoEasyWorld <70003713+apple3243@users.noreply.github.com>
2023-08-16 14:58:50 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
-const useClick = (onClick) => {
-  // useRef is a React Hook that lets you reference a value that’s not needed for rendering.
-  const element = useRef();
-  useEffect(() => {
-    // useEffect will be called when it is in the state of componentDidmount, conponentDidUpdate
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
+const useNetwork = (onChange) => {
+  // navigator.online can tell you true or false depending on your website is online or offline
+  const [status, setStatus] = useState(navigator.onLine);
+  const handleChange = () => {
+    if (typeof onChange === "function") {
+      setStatus(navigator.onLine);
     }
-    // Need to clean up EventListener when it is unmount ~
-    // This function with return will be called when it is in the state of componentWillUnMount
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
+  };
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+    () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
     };
-    // If there had dependency, the function will be called only when it is in the state of componentDidmount
   }, []);
-  return element;
+  return status;
 };
+
 export default function App() {
-  const sayHello = () => (title.current.innerText = "Say Hello");
-  const title = useClick(sayHello);
+  const handleNetworkChange = (online) => {
+    console.log(online ? "We just went online" : "We are offline");
+  };
+  const onLine = useNetwork(handleNetworkChange);
   return (
     <div className="App">
-      <h1 ref={title}>Hello</h1>
+      <h1>{onLine ? "Online" : "Offline"}</h1>
     </div>
   );
 }
