610edb71d6c780657481ddd2f331454bcc53cc2d
NoEasyWorld <70003713+apple3243@users.noreply.github.com>
2023-08-18 09:57:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
-const useClick = (onClick) => {
-  // useRef is a React Hook that lets you reference a value that’s not needed for rendering.
-  const element = useRef();
-  useEffect(() => {
-    // useEffect will be called when it is in the state of componentDidmount, conponentDidUpdate
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
     }
-    // Need to clean up EventListener when it is unmount ~
-    // This function with return will be called when it is in the state of componentWillUnMount
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
-    };
-    // If there had dependency, the function will be called only when it is in the state of componentDidmount
-  }, []);
-  return element;
+  };
+  return fireNotif;
 };
+
 export default function App() {
-  const sayHello = () => (title.current.innerText = "Say Hello");
-  const title = useClick(sayHello);
+  const triggerNotif = useNotification("Can I steal your kimchi", {
+    body: "I love Kimchi don't you?",
+    badge:
+      "https://fastly.picsum.photos/id/431/200/300.jpg?hmac=aUpIWBq8svIaK2ruTnNG-BZuvcDsK9Mr9PuJuYAYEQ0",
+  });
   return (
     <div className="App">
-      <h1 ref={title}>Hello</h1>
+      <button onClick={triggerNotif}>Hello</button>
     </div>
   );
 }
