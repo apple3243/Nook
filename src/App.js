172b10287654545f6d6fb6fb440583ed6e63f9d6
NoEasyWorld <70003713+apple3243@users.noreply.github.com>
2023-08-18 12:57:35 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
-import defaultAxios from "axios";
-import { useEffect, useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import "./styles.css";
+import useAxios from "./useAxios";
 
-const useAxios = (opts, axiosInstance = defaultAxios) => {
-  const [state, setState] = useState({
-    loading: true,
-    error: null,
-    data: null,
+export default function App() {
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json",
   });
-  const [trigger, setTrigger] = useState(0);
-  if (!opts.url) {
-    return;
-  }
-  const refetch = () => {
-    setState({
-      ...state,
-      loading: true,
-    });
-    setTrigger(Date.now());
-  };
-  useEffect(() => {
-    axiosInstance(opts)
-      .then((data) => {
-        setState({
-          ...state,
-          loading: false,
-          data,
-        });
-      })
-      .catch((error) => {
-        setState({ ...state, loading: false, error });
-      });
-  }, [trigger]);
-  return { ...state, refetch };
-};
-
-export default useAxios;
+  console.log(
+    `Loading: ${loading}\nError:${error}\nData:${JSON.stringify(data)}`
+  );
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <h2>{error && "error!!"}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
+}
